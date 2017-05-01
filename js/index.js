be1aa98e4a736b152a9dcc1629ae018b874f3178
Nick <tannernc.nt@gmail.com>
2017-04-30 21:57:44 -0400

    Had to update api to Quotzzy

--- js/index.js
+++ js/index.js
@@ -1,46 +1,42 @@
 $(document).ready(function() {
-  
-	var quote, author;
-  function newQuoteAndAuthor() {
-		$.ajax({
-			url: 'http://api.forismatic.com/api/1.0/', //get quotes from forismatic api in jsonp format
-			jsonp: 'jsonp',
-			dataType: 'jsonp',
-			data: {
-				method: 'getQuote',
-				lang: 'en',
-				format: 'jsonp',
-			},
-			success: function(response) {
-				author = response.quoteAuthor;
-				quote = response.quoteText;
-				$('#quote').text(quote); //set 'p' id to quote
-				if (author) { //if there's an author, set 'footer' id to author
-					$('#saidBy').text(author);
-				} else { //if no author, set id to author unknown
-					$('#saidBy').text('Unknown');
-				}
-			}
-		});
-	}
-//Get quote
-	$('#getQuote').on('click', function() {
+    var quote, author;
 
-		$('.new').fadeOut(100).fadeIn(800);
-		$('#saidBy').fadeOut(100).fadeIn(2000);
+    function newQuoteAndAuthor() {
+        $.ajax({
+            url: 'http://www.quotzzy.co/api/quote', // get quote and author
+            success: function(response) {
+                author = response.author.name;
+                quote = response.text;
 
-		newQuoteAndAuthor();
+                $('#quote').text(quote); //set 'p' id to quote
 
-		var backgroundColorChange = ["#f2d5d5", "#efd5f2", "#d6d5f2", "#d5e5f2", "#c7f9f4", "#c7f9e2", "#c8f9c7", "#ecf9c7", "#f9f5c7", "#f9e4c7", "#f9cfc7"]; //set an array of colors for background change
-		var randColor = Math.floor(Math.random() * backgroundColorChange.length); //Randomize color
- 
-		$('body').animate({
-      'backgroundColor': backgroundColorChange[randColor]
-    }, 2000);
-    //Tweet it
-    $('#tweet').on('click', function() { //Open a new window when tweet button is clicked with appropriate variable infromation
-			window.open('https://twitter.com/intent/tweet/?text=' + encodeURIComponent(quote + ' -by ' + author), "", 'width=600,height=450');
-		});
-	});
+                if (author) { //if there's an author, set 'footer' id to author
+                    $('#saidBy').text(author);
+                } else { //if no author, set id to author unknown
+                    $('#saidBy').text('Unknown');
+                }
+            },
+            error: function(status, error) {
+                console.log(status, error);
+            }
+        });
+    }
+    //Get quote
+    $('#getQuote').on('click', function() {
+        $('.new').fadeOut(100).fadeIn(800);
+        $('#saidBy').fadeOut(100).fadeIn(2000);
 
+        newQuoteAndAuthor();
+
+        var backgroundColorChange = ["#f2d5d5", "#efd5f2", "#d6d5f2", "#d5e5f2", "#c7f9f4", "#c7f9e2", "#c8f9c7", "#ecf9c7", "#f9f5c7", "#f9e4c7", "#f9cfc7"]; //set an array of colors for background change
+        var randColor = Math.floor(Math.random() * backgroundColorChange.length); //Randomize color
+
+        $('body').animate({
+            'backgroundColor': backgroundColorChange[randColor]
+        }, 2000);
+        //Tweet it
+        $('#tweet').on('click', function() { //Open a new window when tweet button is clicked with appropriate variable information
+            window.open('https://twitter.com/intent/tweet/?text=' + encodeURIComponent(quote + ' -by ' + author), "", 'width=600,height=450');
+        });
+    });
 });
