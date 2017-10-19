2ae8fb3de997c677ebc3d399db25a960ff7b354a
Nick <tannernc.nt@gmail.com>
2017-10-18 21:11:53 -0400

    had to update and use forismatic api

--- js/index.js
+++ js/index.js
@@ -3,12 +3,19 @@ $(document).ready(function() {
 
     function newQuoteAndAuthor() {
         $.ajax({
-            url: 'http://www.quotzzy.co/api/quote', // get quote and author
+            url: "https://api.forismatic.com/api/1.0/",
+            jsonp: "jsonp",
+            dataType: "jsonp",
+            data: {
+                method: "getQuote",
+                lang: "en",
+                format: "jsonp"
+            },
             success: function(response) {
-                author = response.author.name;
-                quote = response.text;
 
-                $('#quote').text(quote); //set 'p' id to quote
+                author = response.quoteAuthor;
+                quote = response.quoteText;
+                $('#quote').text(quote); 
 
                 if (author) { //if there's an author, set 'footer' id to author
                     $('#saidBy').text(author);
@@ -27,15 +34,15 @@ $(document).ready(function() {
         $('#saidBy').fadeOut(100).fadeIn(2000);
 
         newQuoteAndAuthor();
-
-        var backgroundColorChange = ["#f2d5d5", "#efd5f2", "#d6d5f2", "#d5e5f2", "#c7f9f4", "#c7f9e2", "#c8f9c7", "#ecf9c7", "#f9f5c7", "#f9e4c7", "#f9cfc7"]; //set an array of colors for background change
+        //set an array of colors for background change
+        var backgroundColorChange = ["#f2d5d5", "#efd5f2", "#d6d5f2", "#d5e5f2", "#c7f9f4", "#c7f9e2", "#c8f9c7", "#ecf9c7", "#f9f5c7", "#f9e4c7", "#f9cfc7"]; 
         var randColor = Math.floor(Math.random() * backgroundColorChange.length); //Randomize color
 
         $('body').animate({
             'backgroundColor': backgroundColorChange[randColor]
         }, 2000);
         //Tweet it
-        $('#tweet').on('click', function() { //Open a new window when tweet button is clicked with appropriate variable information
+        $('#tweet').on('click', function() { //Open a new window when tweet button is clicked with appropriate variable infromation
             window.open('https://twitter.com/intent/tweet/?text=' + encodeURIComponent(quote + ' -by ' + author), "", 'width=600,height=450');
         });
     });
